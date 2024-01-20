e20852387f23eb6b1ec4017b80070d91eb14d02d
FrancescoConso <francesco.consonni1727@gmail.com>
2024-01-20 16:17:09 +0100

    Converted to single-page website with internal React logic

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,6 @@ export function Footer() {
           <p>
             This website was developed with <a href="https://react.dev/">React</a> - css base provided by <a href="http://getskeleton.com/">Skeleton</a> - icons provided by <a href="https://cdnlogo.com/">cdnlogo</a> 
           </p>
-          <p>
-            Sure, using React for a static website may be like using a cannon to kill a mosquito, but I really like the modularity React provides.
-          </p>
         </footer>
       </div>
     </>
--- src/components/WebsiteBody.tsx
+++ src/components/WebsiteBody.tsx
@@ -1,29 +1,49 @@
-import { Link, Routes, Route } from "react-router-dom";
-
 import { NotFound } from "./NotFound";
 import { Curriculum } from "../Pages/Curriculum";
 import { Hobby } from "../Pages/Hobby";
 import { Contact } from "../Pages/Contact";
-
+import { useState } from "react";
 
 export function WebsiteBody() {
+  const [page, setPage] = useState<string>("/");
+
+  function renderSwitch(page: string) {
+    switch (page) {
+      case "/":
+        return <Curriculum></Curriculum>;
+        break;
+      case "/hobby":
+        return <Hobby></Hobby>;
+        break;
+      case "/contact":
+        return <Contact></Contact>;
+        break;
+      default:
+        return <NotFound></NotFound>;
+        break;
+    }
+  }
+
   return (
     <>
       <div className="container with-bottom-border">
         <div className="row center-text">
           <div className="three columns">
             <h5>
-              <Link to="/">Curriculum</Link>
+              {/*<Link to="/">Curriculum</Link>*/}
+              <a onClick={() => setPage("/")}>Curriculum</a>
             </h5>
           </div>
           <div className="three columns">
             <h5>
-              <Link to="/hobby">Hobby Projects</Link>
+              {/* <Link to="/hobby">Hobby Projects</Link> */}
+              <a onClick={() => setPage("/hobby")}>Hobby Projects</a>
             </h5>
           </div>
           <div className="three columns">
             <h5>
-              <Link to="/contact">Contact</Link>
+              {/* <Link to="/contact">Contact</Link> */}
+              <a onClick={() => setPage("/contact")}>Contact</a>
             </h5>
           </div>
           <div className="three columns">
@@ -31,12 +51,13 @@ export function WebsiteBody() {
           </div>
         </div>
       </div>
-      <Routes>
+      {renderSwitch(page)}
+      {/* <Routes>
         <Route path="/" element={<Curriculum />}></Route>
         <Route path="/hobby" element={<Hobby />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/*" element={<NotFound />}></Route>
-      </Routes>
+      </Routes> */}
     </>
   );
 }
